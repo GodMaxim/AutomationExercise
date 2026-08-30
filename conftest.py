82a162ea4a930eb21ce4d67552cd6b9cf2c63db3
GodMaxim <max.zabashta.c4@gmail.com>
2026-08-31 01:30:03 +0300

    Config changes

--- conftest.py
+++ conftest.py
@@ -46,7 +46,11 @@ def page(request):
             locale="en-US",
             extra_http_headers={"Accept-Language": "en-US,en;q=0.9"},
             viewport={"width": 1366, "height": 768},
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            user_agents = {
+                "chromium": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "firefox": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+                "webkit": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15"
+                }
         )
         context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
